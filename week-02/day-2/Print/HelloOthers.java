eda0995a0df968c2ea7aaa9c250b5c897e93b36f
attilasepsi <attilasepsi@gmail.com>
2021-10-20 22:28:48 +0200

    This is a new version of my week-02

--- week-02/day-2/Print/HelloOthers.java
+++
@@ -1,9 +0,0 @@
-package Print;
-
-public class  HelloOthers {
-    public static void main(String[] args) {
-        System.out.println("Hello, Eszter!");
-        System.out.println("Hello, Dóra!");
-        System.out.println("Hello, László!");
-    }
-}
--- week-02/day-2/Print/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package Print;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Kiírja a konzolra, hogy "Hello, World!"
-        System.out.println("Hello, World!");
-    }
-}
