51b276b651dc0a63f9d5ac85cf454261e5bd99dd
attilasepsi <attilasepsi@gmail.com>
2021-10-21 16:02:15 +0200

    This is a new Loop exercise

---
+++ week-02/day-4/Loops/DrawChessTable.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.Loops;
+
+public class DrawChessTable {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 4) {
+            System.out.println("% % % %");
+            System.out.println(" % % % %");
+            a += 1;
+        }
+    }
+}
