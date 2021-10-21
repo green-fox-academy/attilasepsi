50ebb0758420aa55fe22f585df7aa489fbc28248
attilasepsi <attilasepsi@gmail.com>
2021-10-21 19:56:19 +0200

    Here is the next one

---
+++ week-02/day-4/Arrays/SumElements.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.Arrays;
+
+public class SumElements {
+    public static void main(String[] args) {
+        // - Készíts egy `numbers` nevű változót (tömböt)
+        //   a következő tartalommal (értékekkel): `[54, 23, 66, 12]`
+        // - Írasd ki a konzolra a `numbers` nevű tömb második és harmadik elemének összegét
+
+        int[] numbers = {54, 23, 66, 12};
+        System.out.println("Összeg: " + (numbers[1] + numbers[2]));
+
+    }
+}
