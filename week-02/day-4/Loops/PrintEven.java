fc2ec44582db059b6df5479b36a7754c88710778
attilasepsi <attilasepsi@gmail.com>
2021-10-21 15:34:39 +0200

    I changed the location of the directory

---
+++ week-02/day-4/Loops/PrintEven.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.Conditionals;
+
+public class PrintEven {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiírja az összes páros számot 0 és 500 között
+        int a = 0;
+        while (a < 502) {
+            System.out.println(a);
+            a += 2;
+        }
+    }
+}
