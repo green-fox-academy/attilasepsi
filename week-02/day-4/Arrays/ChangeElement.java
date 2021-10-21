d381bbf456f4e6a8670f039e3d068bcd8950e2e5
attilasepsi <attilasepsi@gmail.com>
2021-10-21 20:08:46 +0200

    I've added the next array

---
+++ week-02/day-4/Arrays/ChangeElement.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.Arrays;
+
+public class ChangeElement {
+    public static void main(String[] args) {
+        // - Készíts egy `numbers` nevű változót (tömböt)
+        //   a következő tartalommal (értékekkel): `[1, 2, 3, 8, 5, 6]`
+        // - Változtasd meg a 8-ast 4-esre
+        // - Írasd ki a konzolra a `numbers` nevű tömb negyedik elemét
+        int[] numbers = {1, 2, 3, 8, 5, 6};
+        numbers[3] = 4;
+        System.out.println("A tömb negyedik eleme: " + numbers[3]);
+    }
+}
