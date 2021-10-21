f95b75e07366d17a96214bbd2f09a500c0977acb
attilasepsi <attilasepsi@gmail.com>
2021-10-21 15:11:27 +0200

    Conditionals

---
+++ week-02/day-3/Conditionals/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.Conditionals;
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        // Írj egy programot ami 100-szor kiírja:
+        // "Nem fogok csalni az a vizsgán!"
+
+        for (int i = 0; i < 101; i++) {
+            System.out.println("Nem fogok csalni az a vizsgán!");
+        }
+    }
+}
