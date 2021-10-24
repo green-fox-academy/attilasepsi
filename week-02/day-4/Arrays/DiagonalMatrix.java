f8e597ab6ebe1b66a96c06574ac68bf467832506
attilasepsi <attilasepsi@gmail.com>
2021-10-24 16:57:08 +0200

    I've added new task

---
+++ week-02/day-4/Arrays/DiagonalMatrix.java
@@ -0,0 +1,37 @@
+package com.greenfoxacademy.Arrays;
+
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+
+        // - Készíts (dinamikusan*) egy kétdimenziós tömböt
+//   a következő mátrix-szal**. Ciklust (loop-ot) használj!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Írasd ki a konzolra ezt a kétdimenziós tömböt
+// * A tömb hossza egy változótól kell függjön
+// ** Nyugalom, egy mátrix pont olyan, mint egy tömb
+
+        int[][] matrix = new int[4][4];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (row == col) {
+                    matrix[row][col] = 1;
+                } else {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ week-02/day-5/Functions/Doubling.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.Functions;
+
+public class Doubling {
+    public static void main(String[] args) {
+        // - Hozz létre egy `baseNum` nevű integer változót, aminek az értéke legyen `123`.
+
+// - Készíts egy függvényt `doubling` névvel, mely megduplázza a függvény
+//   bemeneti paraméterét és integer-ként adja vissza(return).
+
+// - Írasd ki a visszatérési értékét a `doubling(baseNum)` függvénynek.
+
+        int baseNum = 123;
+        System.out.println("A visszatérési érték: " + doubling(baseNum) * 2);
+    }
+
+    public static int doubling(int baseNum) {
+        return baseNum;
+    }
+}   
